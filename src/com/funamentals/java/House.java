65c243203220da3612eec325be359f0d91e0c7a2
Brandon Jordan <Bjorda73@gmail.com>
2020-04-23 13:41:36 -0500

    Lesson 4 - My Application Overview

---
+++ src/com/funamentals/java/House.java
@@ -0,0 +1,13 @@
+package com.funamentals.java;
+
+public class House {
+    String foundation;
+    String rooftop;
+    String doorcolor;
+    int windowSize;
+
+    public void doorOpenClose() {
+        System.out.println("The door is open");
+    } // end method doorOpenClose
+
+} // end class House
--- src/com/funamentals/java/Main.java
+++ src/com/funamentals/java/Main.java
@@ -1,5 +1,7 @@
 package com.funamentals.java;
 
+/* This class is part of Lesson 4
+* My Application Overview */
 public class Main {
 
     public static void main(String[] args) {
@@ -9,5 +11,12 @@ public class Main {
         System.out.println("I am new to java");
         System.out.println("This Java course is cool");
         System.out.println("I am learning stuff everyday");
-    }
-}
+
+        myHouseExample();
+    } //End main method
+
+    public static void myHouseExample() {
+        House myHouse = new House();
+        myHouse.doorOpenClose();
+    } // End myHouseExample
+} // end class Main
