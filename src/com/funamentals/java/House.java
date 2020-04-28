7dc8b9112f428a36086ddce8a81f37db4d694934
Brandon Jordan <Bjorda73@gmail.com>
2020-04-28 15:34:16 -0500

    Lesson 4 My Application Review

--- src/com/funamentals/java/House.java
+++ src/com/funamentals/java/House.java
@@ -1,13 +1,4 @@
 package com.funamentals.java;
 
 public class House {
-    String foundation;
-    String rooftop;
-    String doorcolor;
-    int windowSize;
-
-    public void doorOpenClose() {
-        System.out.println("The door is open");
-    } // end method doorOpenClose
-
-} // end class House
+}
--- src/com/funamentals/java/Main.java
+++ src/com/funamentals/java/Main.java
@@ -1,22 +1,21 @@
 package com.funamentals.java;
 
-/* This class is part of Lesson 4
-* My Application Overview */
 public class Main {
 
     public static void main(String[] args) {
-	//write your code here
+        // single line comment
         System.out.println("Hello Java");
-        System.out.println("This is my first sentence");
-        System.out.println("I am new to java");
-        System.out.println("This Java course is cool");
-        System.out.println("I am learning stuff everyday");
-
         myHouseExample();
-    } //End main method
+    }// end method main
 
-    public static void myHouseExample() {
+    /* This is a
+    multi-line comment */
+     public static void myHouseExample() {
         House myHouse = new House();
         myHouse.doorOpenClose();
-    } // End myHouseExample
+     }// end method myHouseExample
+
 } // end class Main
+
+
+
