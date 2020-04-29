e6b9705d6e44edf6cbe6b3fa9b7bee31289589c3
Brandon Jordan <Bjorda73@gmail.com>
2020-04-29 15:22:04 -0500

    Lesson 5 Primitive Data Types

--- src/com/funamentals/java/House.java
+++ src/com/funamentals/java/House.java
@@ -1,4 +1,13 @@
 package com.funamentals.java;
 
 public class House {
+    String foundation;
+    String rootType;
+    String doorColor;
+    int windowsize;
+
+    public void doorOpenClose() {
+        System.out.println("The door opens");
+    }
+
 }
--- src/com/funamentals/java/Main.java
+++ src/com/funamentals/java/Main.java
@@ -3,10 +3,28 @@ package com.funamentals.java;
 public class Main {
 
     public static void main(String[] args) {
+        lesson5Examples();
         // single line comment
-        System.out.println("Hello Java");
-        myHouseExample();
+        //System.out.println("Hello Java");
+        //System.out.println(This is my first sentence);
+        //System.out.println(I am new to Java);
+        // System.out.println(This Java course is cool);
+        //System.out.println(I am learning new stuff every day);
+
+
+
+        //myHouseExample();
     }// end method main
+
+    public static void  lesson5Examples() {
+        Lesson5 myFive = new Lesson5();
+        myFive.showLocalExample();
+        int example = myFive.showLocalWithReturn(5,7); // return to a variable
+        System.out.println(example); // print variable from line above
+        System.out.println(myFive.showLocalWithReturn(2,9)); // direct return of int
+        myFive.showInstanceVariable();
+        myFive.showConversion();
+    } // end method lesson5Examples
 
     /* This is a
     multi-line comment */
@@ -15,7 +33,7 @@ public class Main {
         myHouse.doorOpenClose();
      }// end method myHouseExample
 
-} // end class Main
+} // End class Main
 
 
 
